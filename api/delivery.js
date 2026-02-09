2a89d62d24044be199dc8eb5443ebea6435753fd
saifeldeen28 <said.nada2809@gmail.com>
2026-02-09 10:39:48 +0200

    Reduce sleep duration from 1000ms to 1ms

--- api/delivery.js
+++ api/delivery.js
@@ -257,7 +257,7 @@ export default async function handler(req, res) {
         status: result 
       });
       
-      await sleep(1000); 
+      await sleep(1); 
     }
 
     return res.status(200).json({ success: true, dispatch_results: results });
